74e59798ce083fad605488254c8df0d07d2e676e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2021-04-13 01:36:13 +0200

    Rystem v.1.0.0 RC

--- Rystem.UnitTest/Crypting.cs
+++ Rystem.UnitTest/Crypting.cs
@@ -39,6 +39,32 @@ namespace Rystem.UnitTest
             string crypting2 = falseNueve.ToHash();
             Assert.Equal(crypting1, crypting2);
         }
+        [Fact]
+        public void Base45()
+        {
+            var falseNueve = new FalseNueve()
+            {
+                Al = "a",
+                Ol = "b"
+            };
+            string crypting1 = falseNueve.ToBase45();
+            string crypting2 = falseNueve.ToBase45();
+            Assert.Equal(crypting1, crypting2);
+            Assert.Equal(crypting1.FromBase45<FalseNueve>().Al, crypting2.FromBase45<FalseNueve>().Al);
+        }
+        [Fact]
+        public void Base64()
+        {
+            var falseNueve = new FalseNueve()
+            {
+                Al = "a",
+                Ol = "b"
+            };
+            string crypting1 = falseNueve.ToBase64();
+            string crypting2 = falseNueve.ToBase64();
+            Assert.Equal(crypting1, crypting2);
+            Assert.Equal(crypting1.FromBase64<FalseNueve>().Al, crypting2.FromBase64<FalseNueve>().Al);
+        }
         private class FalseNueve
         {
             public string Al { get; set; }
