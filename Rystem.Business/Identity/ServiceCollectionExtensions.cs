276fd9be28471f2680d559d4ce59372e4a67bcc6
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2021-11-08 19:11:59 +0100

    x

---
+++ Rystem.Business/Identity/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rystem.Business;
+
+namespace Rystem
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        //public static RystemDocumentServiceProvider<TEntity> AddStorageIdentity(this IServiceCollection services)
+        //    where TEntity : new()
+        //    => new(services);
+    }
+}
